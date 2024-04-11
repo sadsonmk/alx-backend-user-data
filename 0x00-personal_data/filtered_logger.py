66c1cf6206f6f6cf4ca0e475a78c1cc2c0443295
Sadsonmk <sadsonmkumira@yahoo.com>
2024-04-10 22:24:04 -0700

    Adding files

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -8,21 +8,6 @@ import re
 
 def filter_datum(fields: str, redaction: str, message: str, separator: str):
     """a function that returns the log message obfuscated
-
-        Args:
-            fields(str): a list of strings representing all
-            fields to obfuscate
-
-            redaction(str): a string representing by what the
-            field will be obfuscated
-
-            message(str): a string representing the log line
-
-            separator(str): a string representing by which character
-            is separating all fields in the log line (message)
-
-        Return:
-            returns the log message obfuscated
     """
     for val in fields:
         message = re.sub(val + '=.*?' + separator, val + '=' + redaction
