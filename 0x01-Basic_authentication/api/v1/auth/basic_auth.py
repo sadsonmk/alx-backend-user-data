06f2543cbfcb1191f47185917ce89cc4b78997cf
Sadsonmk <sadsonmkumira@yahoo.com>
2024-04-16 12:31:49 -0700

    Adding file 10. Basic - User object

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 """This is a module that contains the class BasicAuth"""
 
-
+from models.user import User
 from api.v1.auth.auth import Auth
 import base64
+from typing import (
+        TypeVar,
+        )
 
 
 class BasicAuth(Auth):
@@ -50,3 +53,20 @@ class BasicAuth(Auth):
             return None, None
         else:
             return tuple(decoded_base64_authorization_header.split(':'))
+
+    def user_object_from_credentials(self, user_email: str, user_pwd:
+                                     str) -> TypeVar('User'):
+        """returns the User instance based on his email and password"""
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+        try:
+            match = User.search({'email': user_email})
+        except Exception:
+            return None
+
+        for user in match:
+            if user.is_valid_password(user_pwd):
+                return user
+        return None
