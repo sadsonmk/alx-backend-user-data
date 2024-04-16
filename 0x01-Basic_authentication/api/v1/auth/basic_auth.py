a00c2501ee17c556564fb7a641de044f8a3af0e1
Sadsonmk <sadsonmkumira@yahoo.com>
2024-04-16 11:13:52 -0700

    adding files 9. Basic - User credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -36,3 +36,17 @@ class BasicAuth(Auth):
             return decoded.decode('utf-8')
         except (TypeError, base64.binascii.Error):
             return None
+
+    def extract_user_credentials(self, decoded_base64_authorization_header:
+                                 str) -> (str, str):
+        """returns the user email and password from
+            the Base64 decoded value
+        """
+        if not decoded_base64_authorization_header:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+        else:
+            return tuple(decoded_base64_authorization_header.split(':'))
