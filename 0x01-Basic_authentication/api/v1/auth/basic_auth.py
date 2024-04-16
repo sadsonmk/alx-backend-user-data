eb8eab164eef135cb9e9ac4900af983203c12075
Sadsonmk <sadsonmkumira@yahoo.com>
2024-04-16 11:03:45 -0700

    Adding file 8. Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,14 +3,15 @@
 
 
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
     """inherits from the Auth class"""
     def extract_base64_authorization_header(self, authorization_header:
                                             str) -> str:
-        """def extract_base64_authorization_header(self,
-        authorization_header: str) -> str:
+        """returns the Base64 part of the Authorization
+            header for a Basic Authentication
         """
         if not authorization_header:
             return None
@@ -20,3 +21,18 @@ class BasicAuth(Auth):
             return None
         else:
             return authorization_header.split(' ')[1]
+
+    def decode_base64_authorization_header(self, base64_authorization_header:
+                                           str) -> str:
+        """returns the decoded value of a Base64 string
+            base64_authorization_header
+        """
+        if not base64_authorization_header:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded = base64.b64decode(base64_authorization_header)
+            return decoded.decode('utf-8')
+        except (TypeError, base64.binascii.Error):
+            return None
