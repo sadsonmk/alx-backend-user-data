4a2bbe9101a920354c7004aafae38ac48a855ac5
Sadsonmk <sadsonmkumira@yahoo.com>
2024-04-16 10:31:35 -0700

    Adding the basic_auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""This is a module that contains the class BasicAuth"""
+
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """inherits from the Auth class"""
+    pass
