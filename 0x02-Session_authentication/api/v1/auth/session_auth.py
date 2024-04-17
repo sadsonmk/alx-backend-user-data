8de169e9559cdaa44b354c2592d582296c286a5e
Sadsonmk <sadsonmkumira@yahoo.com>
2024-04-17 10:59:39 -0700

    Adding file 2. Create a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,8 +2,17 @@
 """This is a module for a class SessionAuth that inherits from Auth"""
 
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """creates a sessionAuth and inherits from the Auth class"""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """creates a Session ID for a user_id"""
+        if not user_id or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
