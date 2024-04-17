9d5436d041a53c68bd7318e9ac7196addf2fda18
Sadsonmk <sadsonmkumira@yahoo.com>
2024-04-17 10:41:38 -0700

    Adding 1. Empty session

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""This is a module for a class SessionAuth that inherits from Auth"""
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """creates a sessionAuth and inherits from the Auth class"""
+    pass
