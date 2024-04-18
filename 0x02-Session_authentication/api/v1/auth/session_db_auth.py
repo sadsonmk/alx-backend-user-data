b43b30e498317061799fece1daac43e3669a4e18
Sadsonmk <sadsonmkumira@yahoo.com>
2024-04-18 11:40:34 -0700

    10. Sessions in database

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -10,7 +10,7 @@ from models.user_session import UserSession
 class SessionDBAuth(SessionExpAuth):
     """inherits from SessionExpAuth and stores sessions in a database"""
 
-    def create_session(self, user_id=None):
+    def create_session(self, user_id=None) -> str:
         """creates and stores new instance of UserSession
             and returns the Session ID
         """
@@ -24,7 +24,7 @@ class SessionDBAuth(SessionExpAuth):
             user_sess.save()
             return session_id
 
-    def user_id_for_session_id(self, session_id=None):
+    def user_id_for_session_id(self, session_id=None) ->str:
         """returns the User ID by requesting UserSession
             in the database based on session_id
         """
@@ -36,7 +36,7 @@ class SessionDBAuth(SessionExpAuth):
             return None
         now = datetime.now()
         delta = timedelta(seconds=self.session_duration)
-        if session[0].created_at + delta < now:
+        if (session[0].created_at + delta) < now:
             return None
         return session[0].user_id
 
