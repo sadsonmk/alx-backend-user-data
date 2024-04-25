8e11382351f476d5c16e6ee734f40a1b9e240bb3
Sadsonmk <sadsonmkumira@yahoo.com>
2024-04-25 09:10:58 -0700

    creating a route session in app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -33,15 +33,12 @@ def login():
     """
     email = request.form.get('email')
     password = request.form.get('password')
-    try:
-        valid_logn = auth.valid_login(email, password)
-        if valid_logn:
-            res = make_response(jsonify({"email": email, "message": "logged in"}))
-            res.set_cookie('session_id', auth.create_session(email))
-            return res
-        abort(401)
-    except Exception:
-        abort(401)
+    valid_logn = auth.valid_login(email, password)
+    if valid_logn:
+        res = make_response(jsonify({"email": email, "message": "logged in"}))
+        res.set_cookie('session_id', auth.create_session(email))
+        return res
+    abort(401)
 
 
 if __name__ == "__main__":
