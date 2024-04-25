7ca2f7ff5c1b0a101f465dc0b8b142bcba1e0e40
Sadsonmk <sadsonmkumira@yahoo.com>
2024-04-25 09:49:26 -0700

    creating a route session in app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -32,7 +32,7 @@ def users():
         return jsonify({"message": "email already registered"}), 400
 
 
-@app.route('/sessions', methods=['POST'])
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
 def login():
     """create a new session for the user,
         store it the session ID as a cookie
